ea0373f803836b5fe0c76e6462e13ed7b23afcbe
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-22 22:12:50 -0700

    remove fetch

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function fetchData(){
-  fetch('https://dry-hollows-79406.herokuapp.com/api/v1/forecast')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => console.error(error))
-}
-fetchData();
-console.log("hmmm");
+// function fetchData(){
+//   fetch('https://dry-hollows-79406.herokuapp.com/api/v1/forecast')
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(error => console.error(error))
+// }
+// fetchData();
+console.log("Index connected");
