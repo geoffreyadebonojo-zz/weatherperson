5f1fc1de708d73da2f523f388c7c7b8803d401e9
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-24 09:52:25 -0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
+// username: example@mail.com
+// password: testpassword123
+// api-key: b7808ade-0e79-4992-bca2-38347aa2a1e3
+
 console.log("script.js loaded");
-function fetchData(){
-  fetch('https://dry-hollows-79406.herokuapp.com/api/v1/forecast')
-    .then(response => response.json())
-    .then(data => {
-      localStorage.setItem("forecastData", JSON.stringify(data));
-    })
-    .catch(error => console.error(error))
-}
-var loadButton = document.getElementById("load-button");
-loadButton.addEventListener("click", fetchData());
+
+$("#button-1").click(function(){
+  $.ajax({
+    url: "https://dry-hollows-79406.herokuapp.com/api/v1/forecast",
+    dataType: "json",
+    type: "get",
+    contentType: "application/json",
+    success: function(result){
+      $("#div1").html(result);
+    }
+  });
+});
+
+console.log(
+
+$("#button-2").click(function(){
+  $.ajax({
+    url: "https://dry-hollows-79406.herokuapp.com/api/v1/favorites",
+    dataType: "json",
+    type: "get",
+    contentType: "application/json",
+    data: { api_key: "b7808ade-0e79-4992-bca2-38347aa2a1e3" },
+    success: function( data, textStatus, errorThrown ){
+      console.log( data );
+      document.getElementById("div1").innerHTML = "Hi yall";
+    }
+  });
+}));
+
+$("#button-3").click(function(){
+  $.ajax({
+    url: "https://dry-hollows-79406.herokuapp.com/api/v1/favorites",
+    dataType: "json",
+    type: "post",
+    contentType: "application/json",
+    data:
+    {
+      api_key: "b7808ade-0e79-4992-bca2-38347aa2a1e3",
+      location: "Portland, OR"
+    },
+    success: function( data, textStatus, errorThrown ){
+      console.log( textStatus );
+    }
+  });
+});
