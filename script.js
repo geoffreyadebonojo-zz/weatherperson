5629a8e73e718cfe499673037b12ed022619ba05
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-24 17:32:38 -0700

    add favorite location submit button

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function getWeather(){
-
-
   //if (localStorage.getItem("storedForecast") == null)
   fetch('https://dry-hollows-79406.herokuapp.com/api/v1/forecast')
   .then(response => response.json())
@@ -9,23 +7,48 @@ function getWeather(){
     forecast = JSON.parse(localStorage.getItem("storedForecast"));
     console.log("fetched from api");
     console.log(forecast);
+    renderForecastData();
   })
   .catch(error => console.error(error))
-  //else {
-  //  forecast = JSON.parse(localStorage.getItem("storedForecast"));
-  //  console.log("loaded from memory")
-  //  console.log(forecast);
-  //}
 }
-forecast = JSON.parse(localStorage.getItem("storedForecast"));
-console.log("from memory");
-console.log(forecast.id);
-console.log(forecast);
+
+sessionsUrl = "https://dry-hollows-79406.herokuapp.com/api/v1/sessions"
+sessionsData = {
+  email: "example@gmail.com",
+  password: "123"
+}
+
+function apiKey(){
+  $.post(
+    sessionsUrl,
+    sessionsData,
+    function(data, status){
+    localStorage.setItem("api_key", data.data.key)
+  })
+}
+
+favoritesUrl = "https://dry-hollows-79406.herokuapp.com/api/v1/favorites"
+favoritesData = {
+  api_key: localStorage.getItem("api_key")
+}
+
+function getFavorites(){
+  $.get(favoritesUrl, favoritesData, function(data, status){
+    console.log(data);
+  })
+}
+
+console.log($("#add-field").value);
+
+
+
 
 function weatherIcon(forecast){
   switch(forecast) {
+  case "snow":
+    return '<i class="far fa-snowflake"></i>';
   case "clear-day":
-    return '<i class=\"fas fa-sun\" style=\"font-size:32px\"></i>';
+    return '<i class="fas fa-sun"></i>';
   case "clear-night":
     return '<i class="fas fa-moon"></i>';
   case "partly-cloudy-day":
@@ -38,31 +61,49 @@ function weatherIcon(forecast){
   }
 }
 
-$(document).ready( function(){
-  var favorites = document.getElementById('favorites');
-  favorites.innerHTML = "";
-  for (var i = 0; i < 10; i++){
-    favorites.innerHTML += '<div class="box favorite"> Favorited #' + i + '</div>';
+function renderTemp(){
+  var data = JSON.parse(localStorage.getItem("storedForecast"));
+  var temp = data.temperature;
+  var forecast = data.daily_forecasts[0].icon;
+  $( ".temp-container" ).append(
+    "<h1>" +
+    weatherIcon(forecast) + " " +
+    Math.round(temp) + "&deg" +
+    "</h1>"
+  );
+
+  if (temp < 32) {
+    $( ".temp-container" ).css({
+      "color": "rgb(0, 0, 200)",
+      "position": "relative",
+      "left": "100px"
+    });
+  } else {
+    $( ".temp-container" ).css({
+      "color": "rgb(0, 0, 0)",
+      "position": "relative",
+      "left": "100px"
+    });
+  }
+}
+
+renderTemp();
+
+var nameInput = document.getElementById('name');
+
+document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+  console.log(nameInput.value);
+  addFavoritesUrl = "https://dry-hollows-79406.herokuapp.com/api/v1/favorites"
+  addFavoritesData = {
+    api_key: localStorage.getItem("api_key"),
+    location: nameInput.value
   }
 
-  // document.getElementById('icon').innerHTML = '<img class="gif-icon" src="' + forecast.daily_forecasts[0].gif + '"></img>';
-  document.getElementById('icon').innerHTML = weatherIcon(forecast.daily_forecasts[0].icon);
-  document.getElementById('conditions').innerHTML = forecast.summary;
-  document.getElementById('temp').innerHTML = Math.round(forecast.temperature) + "&deg";
+  $.post(addFavoritesUrl, addFavoritesData, function(data, status){
+      console.log(data);
+  });
+  
+  //prevent the normal submission of the form
+  e.preventDefault();
 
-  sunrise =  new Date(forecast.daily_forecasts[0].sunrise).toLocaleTimeString();
-  console.log(forecast.id);
-  document.getElementById('sunrise').innerHTML += sunrise;
-
-  sunset =  new Date(forecast.daily_forecasts[0].sunset).toLocaleTimeString();
-  document.getElementById('sunset').innerHTML += sunset;
-
-  document.getElementById('precip').innerHTML += forecast.daily_forecasts[0].precipitation*100 + "%";
-  document.getElementById('feeling').innerHTML += forecast.feels_like;
-
-  // document.getElementById('next-hour').innerHTML += forecast.hourly_forecast[0];
-
-  for (var i=0; i < 49; i++){
-    document.getElementById('forecasts').innerHTML +=  weatherIcon(forecast.hourly_forecasts[i].icon)  + "     " + Math.round(forecast.hourly_forecasts[i].temp) + "<br>";
-  }
 });
