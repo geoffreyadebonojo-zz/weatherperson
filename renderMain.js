a734913acf0a4b002e4dd3597fac4e282c68d92b
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-26 08:24:22 -0700

    configure left and right upper boxes to display data wireframes specifies

--- renderMain.js
+++ renderMain.js
@@ -7,19 +7,19 @@ function renderMain(){
   var low = data.low;
 
   var leftMain = $( "#left-main" );
-  leftMain.append(
-    `<p class="summary secondary"> ${weatherIcon(forecast)}&nbsp ${forecast} </p>
-    <h2 class="temp-display">
+  leftMain.append(`
+    <p class="summary secondary"> ${weatherIcon(forecast)}&nbsp ${forecast} </p>
+    <h1 class="temp-display">
 
       ${Math.round(temp)}&deg
-    </h2>
+    </h1>
     <p class="high secondary">
       High: ${Math.round(high)}
     </p>
     <p class="high secondary">
       Low:  ${Math.round(low)}
-    </p>`
-  );
+    </p>
+  `);
   if (temp < 32) {
     $(".temp-display").css({
       "color": "rgb(0, 0, 200)",
@@ -34,8 +34,7 @@ function renderMain(){
   var state = data.state;
 
   var rightMain = $("#right-main");
-  rightMain.append(
-    `
+  rightMain.append(`
     <h3>
       ${city}, ${state}
     </h3>
@@ -45,8 +44,7 @@ function renderMain(){
     <p class="secondary">
       Time, Date
     </p>
-    `
-  );
+  `);
 
 }
 renderMain();
