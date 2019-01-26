1c59627e9ecf62d83713e0bdc764e0198a668f0d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-26 15:59:24 -0700

    add render favorites

---
+++ renderFavorites.js
@@ -0,0 +1,9 @@
+function renderFavorites(){
+
+  var leftDetails = $("#left-details");
+  leftDetails.append(`
+    <h1 class="centered">Test</h1>
+    <p class="secondary centered">Test</p>
+    <br>
+  `);
+}
