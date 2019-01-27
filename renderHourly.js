93bb30506fa9f7d5e66a38582a4cc52dc3110d32
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-27 16:46:01 -0700

    fix hourly forecast time display

--- renderHourly.js
+++ renderHourly.js
@@ -2,10 +2,13 @@ var data  = JSON.parse(localStorage.getItem("storedForecast"));
 
 var start = new Date().getHours();
 console.log(start);
+start = 20;
 var forecasts = []
 for (var i= 0; i<8; i++){
   var hours = new Date(data.hourly_forecasts[i+(start-8)].id*1000).getHours();
-  if (hours < 13){
+  if (hours == 0){
+    hours = "12 AM";
+  } else if (hours < 13){
     hours = hours + " AM";
   } else {
     hours = (hours - 12) + " PM";
@@ -17,7 +20,7 @@ for (var i= 0; i<8; i++){
   box.append(`
     <div
     class="hourly"
-    style="background:rgb(${32 - Math.round(forecast)*5}, 50, ${Math.round(forecast) * 5});">
+    style="background:rgb(${32 - Math.round(forecast)*3}, 50, ${Math.round(forecast) * 5});">
       <p>${hours}</p>
       <p style="position: relative; bottom: ${Math.round(forecast)/2 - 40};"">${Math.round(forecast)}&deg</p>
     </div>
