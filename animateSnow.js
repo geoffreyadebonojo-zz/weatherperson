19b0fe14608fa365b0582e976351c573eb48c456
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-07 12:36:06 -0700

    Add raindrop and conditional to render one or the other based on weather

--- animateSnow.js
+++ animateSnow.js
@@ -77,6 +77,83 @@ function animateSnow() {
 }
 
 
-setInterval(function (){
-  animateSnow();
-}, 1000/30);
+class Raindrop {
+  constructor(x, y, size){
+    this.position= {x: x, y: y};
+    this.velocity= {x: 0, y: 0};
+    this.acceleration= {x: 0, y: 0};
+
+    this.bodySize= Math.random()*size + size;
+    this.maxSpeed= {x: 1, y: this.bodySize};
+    this.limitPosition= {x: 200, y: 400};
+  }
+
+  draw() {
+
+    ctx.beginPath();
+    // ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.font=`${this.bodySize * 3}px FontAwesome`;
+    ctx.fillText("'", this.position.x, this.position.y);
+    ctx.fillStyle = "blue";
+  }
+
+  fall() {
+    this.acceleration.y = 0.1;
+  }
+
+  update() {
+    if (this.velocity.x < this.maxSpeed.x) {
+      this.velocity.x += this.acceleration.x;
+    }
+    this.position.x += this.velocity.x;
+
+    if (this.velocity.y < this.maxSpeed.y) {
+      this.velocity.y += this.acceleration.y;
+    }
+    this.position.y += this.velocity.y;
+  }
+
+  respawn() {
+    if (this.position.y > this.limitPosition.y){
+      this.position.y = 0;
+      this.position.x = Math.random() * canvas.width ;
+    }
+  }
+
+  animate() {
+    this.fall();
+    this.draw();
+    this.update();
+    this.respawn();
+  }
+}
+
+let numDrops = 100;
+
+let drops = [];
+for (var i= 0; i<numDrops; i++){
+  let drop = new Raindrop(Math.random()*800, Math.random()*-1000, 5);
+  drops.push(drop);
+}
+
+function animateRain() {
+  ctx.clearRect(0, 0, 1200, 400);
+  ctx.fillStyle = "#7371FC";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drops.forEach((drop) => {
+    drop.animate();
+  });
+}
+console.log(data.summary);
+let weather = "Light Snow";
+
+if ( weather.includes("Snow") ){
+  setInterval(function (){
+    animateSnow();
+  }, 1000/30);
+} else if ( weather.includes("rain") ){
+  setInterval(function (){
+    animateRain();
+  }, 1000/30);
+}
